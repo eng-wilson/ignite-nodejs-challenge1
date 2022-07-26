2fec2d82e372e9d85e3300e4c4aa4365faeb20d0
Wilson Carvalho <wilson@fuerzastudio.com.br>
2022-07-26 07:54:29 -0300

    feat: update todo

--- src/index.js
+++ src/index.js
@@ -68,7 +68,21 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username)
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo does not exist."})
+  }
+
+  todo.title = title,
+  todo.deadline = deadline
+
+  return response.status(200).json(todo)
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
